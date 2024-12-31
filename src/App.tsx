9e07dcb829b300ad13ed2948997eb66c4ffd8da2
202110861 <dbsalsk2514@naver.com>
2024-12-31 14:30:49 +0900

    chore: prettier 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
-  function heelow() {
-    console.log('hellomworlde');
-  }
+
   return (
     <>
       <div>
