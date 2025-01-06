35856b87261843206b6bc21f6540552e601f05ea
박지현 <wlgusqkr11@naver.com>
2025-01-06 17:47:59 +0900

    Menu컴포넌트주석수정

---
+++ src/shared/@common/ui/Menu/example.tsx
@@ -0,0 +1,14 @@
+import Menu from './Menu';
+
+const Example = () => {
+  return (
+    <Menu
+      title="예시"
+      date="2025/01/06"
+      isReceived={false}
+      onClick={() => {}}
+    />
+  );
+};
+
+export default Example;
