6e7432a893ebfd89e445862c0a71dc15c584d415
박지현 <wlgusqkr11@naver.com>
2025-01-08 21:12:21 +0900

    feat:대관확인메세지페이지,Alert버튼 2개버전 구현(중간커밋)

---
+++ src/pages/User/ReserveList/ReserveList.tsx
@@ -0,0 +1,7 @@
+import ReserveListContainer from '../../../components/User/ReserveList/ReserveListContainer';
+
+const ReserveList = () => {
+  return <ReserveListContainer />;
+};
+
+export default ReserveList;
