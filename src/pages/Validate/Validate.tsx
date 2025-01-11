2fd920aa09ebfc6fb8ef94c6c9384eb6ea1e681e
cgw-notebook <mangang0713@gmail.com>
2025-01-11 12:07:05 +0900

    feat: Validate 페이지 1차 개발

---
+++ src/pages/Validate/Validate.tsx
@@ -0,0 +1,14 @@
+import ValidateForm from '../../components/Validate/ValidateForm';
+import Title from '../../shared/@common/Title';
+
+const Validate = () => {
+  return (
+    <>
+      <div>사업자 인증 페이지입니다.</div>
+      <Title>사업자 인증</Title>
+      <ValidateForm />
+    </>
+  );
+};
+
+export default Validate;
