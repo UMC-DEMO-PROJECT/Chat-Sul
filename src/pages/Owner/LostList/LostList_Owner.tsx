129164aabfc58f32edca8e4344092d5947f2e576
202110861 <dbsalsk2514@naver.com>
2025-01-14 15:39:03 +0900

    feat: 분실물 목록 오너 페이지에 글 작성 버튼 추가

---
+++ src/pages/Owner/LostList/LostList_Owner.tsx
@@ -0,0 +1,35 @@
+import LostList from '../../../components/Lost/Lostlist';
+import TopBar from '../../../shared/ui/TopBar/TopBar';
+import { useNavigate } from 'react-router-dom';
+import LostInput from '../../../components/Lost/LostInput';
+import { useState } from 'react';
+import useDebounce from '../../../hooks/useDebounce';
+import Icon from '../../../shared/ui/Icon/Icon';
+
+const LostListPage_Owner = () => {
+  const navigate = useNavigate();
+  const [mq, setMq] = useState('');
+  const useDebouncedValue = useDebounce(mq, 500);
+
+  return (
+    <div className="flex flex-col justify-center items-center relative">
+      <TopBar
+        title="분실물"
+        onFirstClick={() => navigate('/')}
+        onSecondClick={() => navigate('/user/lost-list')}
+      />
+      <div className="flex flex-col justify-center items-center mt-[25px] ">
+        <LostInput setSearchValue={setMq} />
+        <LostList searchValue={useDebouncedValue} />
+      </div>
+      <button
+        onClick={() => navigate('/owner/lost-form')}
+        className="rounded-full bg-[#CB6015] absolute top-[730px] right-[16px]"
+      >
+        <Icon name="pen" />
+      </button>
+    </div>
+  );
+};
+
+export default LostListPage_Owner;
