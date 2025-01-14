2de7d675b4c4238e580895f380f08f2985064782
cgw-notebook <mangang0713@gmail.com>
2025-01-14 16:13:21 +0900

    refactor: axiosInstance 수정

--- src/shared/api/common/axiosInstance.ts
+++ src/shared/api/common/axiosInstance.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-export const API_URL = import.meta.env.VITE_API_URL;
-
 export const axiosInstance = axios.create({
-  baseURL: API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
 });
+
+console.log('Base URL:', axiosInstance.defaults.baseURL);
