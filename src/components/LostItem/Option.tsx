97bd3d06dfb77df1bac94359547c84f36353675a
202110861 <dbsalsk2514@naver.com>
2025-01-15 15:42:11 +0900

    feat: 분실물 상세페이지 삭제 모달 추가

--- src/components/LostItem/Option.tsx
+++ src/components/LostItem/Option.tsx
@@ -1,9 +1,23 @@
 import { useState, useEffect, useRef } from 'react';
 import Icon from '../../shared/ui/Icon/Icon';
+import ModalLayout from '../../shared/ui/Modal/ModalLayout/ModalLayout';
+import AlertTwoButton from '../../shared/ui/Modal/Alert/AlertTwoButton';
 
 const Option = ({ style }: { style: string }) => {
   const [isClick, setIsClick] = useState(false);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   const ref = useRef<HTMLDivElement>(null);
+
+  const handleDeleteClick = () => {
+    setIsOpen(true);
+  };
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
+  const handleModalDeleteClick = () => {
+    setIsOpen(false);
+  };
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
@@ -31,13 +45,34 @@ const Option = ({ style }: { style: string }) => {
           ref={ref}
           className="flex flex-col justify-center items-center gap-2 p-2 rounded-xl border-[0.2px] border-[rgba(208,208,208,0.43)] absolute top-[52px] right-[12px]"
         >
-          <p className="px-5 py-3 text-[15px] font-normal leading-[20px] tracking-[-0.23px]">
+          <p className="px-5 py-3 text-[15px] font-normal leading-[20px] tracking-[-0.23px] cursor-pointer">
             수정
           </p>
-          <p className="px-5 py-3 text-[15px] font-normal leading-[20px] tracking-[-0.23px]">
+          <p
+            onClick={handleDeleteClick}
+            className="px-5 py-3 text-[15px] font-normal leading-[20px] tracking-[-0.23px] cursor-pointer"
+          >
             삭제
           </p>
         </div>
+      )}
+
+      {isOpen && (
+        <ModalLayout
+          isOpen={isOpen}
+          closeModal={() => {
+            setIsOpen(false);
+          }}
+        >
+          <AlertTwoButton
+            onClick1={handleClose}
+            onClick2={handleModalDeleteClick}
+            btnMessage1="뒤로가기"
+            btnMessage2="삭제하기"
+          >
+            <p>정말로 글을 삭제하시겠습니까?</p>
+          </AlertTwoButton>
+        </ModalLayout>
       )}
     </div>
   );
