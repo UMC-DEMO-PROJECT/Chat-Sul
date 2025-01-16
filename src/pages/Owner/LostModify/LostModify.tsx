8198a66af92b6b7541bc74b1c8e0b5859843348c
202110861 <dbsalsk2514@naver.com>
2025-01-16 15:09:05 +0900

    feat: 분실물 글 수정 페이지 추가 및 nav 경로 수정

---
+++ src/pages/Owner/LostModify/LostModify.tsx
@@ -0,0 +1,26 @@
+import TopBar from '../../../shared/ui/TopBar/TopBar';
+import WritingForm from '../../../components/Lostwriting/LostWritingForm';
+import Button from '../../../shared/ui/Button/button';
+import { useNavigate } from 'react-router-dom';
+
+const LostModifyPage = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col relative">
+      <TopBar
+        title="분실물"
+        onFirstClick={() => navigate('/owner/lost-list')}
+        onSecondClick={() => navigate('/owner/lost-list')}
+      />
+      <WritingForm />
+      <div className="w-[356px] mx-auto absolute top-[760px] left-[24px]">
+        <Button size="large" colorType="filled">
+          수정완료
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default LostModifyPage;
