9d8b0dd4e25dfe9684b5bd8a9a15e872f366d677
박지현 <wlgusqkr11@naver.com>
2025-01-19 14:59:41 +0900

    feat:owner-reserve-list레이아웃완성

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import ReserveForm from './pages/User/ReserveForm/ReserveForm';
 import LostListPage_Owner from './pages/Owner/LostList/LostList_Owner';
 import LostItemPage_Owner from './pages/Owner/LostItem/LostItem_Owner';
 import Validate from './pages/Validate/Validate';
-
+import ReserveListContainer from './components/Owner/ReserveList/ReserveList';
 
 /**
  * '/' : Landing Page, 지도 표시
@@ -56,7 +56,7 @@ const App = () => {
           <Route path="/owner">
             <Route index element={<App />} />
             <Route path="shop" element={<App />} />
-            <Route path="reserve" element={<App />} />
+            <Route path="reserve-list" element={<ReserveListContainer />} />
             <Route path="lost-list" element={<LostListPage_Owner />} />
             <Route path="lost-item/:id" element={<LostItemPage_Owner />} />
             <Route path="lost-form" element={<LostWritingPage />} />
