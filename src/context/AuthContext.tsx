903721aba1293f739f7719fc273878e2f37234b5
202110861 <dbsalsk2514@naver.com>
2025-02-12 16:08:44 +0900

    feat: contextAPI로 accessToken 유무에 따라 로그인 상태 저장

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,43 @@
+import {
+  createContext,
+  useState,
+  useContext,
+  useEffect,
+  PropsWithChildren,
+} from 'react';
+
+type TAuthContext = {
+  isLogin: boolean;
+  setIsLogin: (state: boolean) => void;
+};
+
+const AuthContext = createContext<TAuthContext | null>(null);
+
+export const AuthProvider = ({ children }: PropsWithChildren) => {
+  const [isLogin, setIsLogin] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (localStorage.getItem('accessToken')) setIsLogin(true);
+    else setIsLogin(false);
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLogin,
+        setIsLogin,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export function useAuthContext() {
+  const context = useContext(AuthContext);
+  if (context == null) {
+    throw new Error('AuthProvider를 찾을 수 없습니다.');
+  }
+
+  return context;
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { OwnerProvider } from './context/OwnerContext.tsx';
+import { AuthProvider } from 'context/AuthContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <OwnerProvider>
-      <App />
-    </OwnerProvider>
+    <AuthProvider>
+      <OwnerProvider>
+        <App />
+      </OwnerProvider>
+    </AuthProvider>
   </StrictMode>
 );
